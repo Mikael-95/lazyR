a1162ab883aea0777ec9f519452023e7e5c111e9
Mikael-95 <mikael.a.albrecht@uia.no>
2023-02-28 12:29:51 +0100

    updated not in.

--- R/not_in.R
+++ R/not_in.R
@@ -1,1 +1,17 @@
-`%!in%` = Negate(`%in%`)
+#' Not In
+#'
+#' @param x What to search for
+#' @param y What do search in
+#'
+#' @return a logical vector of whether values of x are not found in y
+#' @export
+#'
+#' @examples
+#' x <- c(1, 2, 3, 4, 5)
+#' y <- c(1, 2, 3, 4, 6)
+#'
+#' x %!in% y
+#'
+`%!in%` <- function(x, y) {
+  !(x %in% y)
+  }
